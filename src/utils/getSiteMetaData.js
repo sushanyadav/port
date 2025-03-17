6163039bbf32c43d0c0646d69c7bc7e54f36788c
Sushan Yadav <sushanyadav98@gmail.com>
2025-03-17 21:02:50 +0545

    init init init

--- src/utils/getSiteMetaData.js
+++ src/utils/getSiteMetaData.js
@@ -3,7 +3,7 @@ const SITE_DESCRIPTION =
   "Frontend Web Developer in Nepal proficient in NextJS, React & JavaScript available for your next website or app.";
 const SITE_KEYWORDS = `react developer in nepal, nextjs developer in nepal, frontend developer in nepal, javascript developer in nepal `;
 const SITE_AUTHOR = "Sushan Yadav";
-const SITE_URL = "https://port-next-gray.vercel.app/";
+const SITE_URL = "https://port-next-gray.vercel.app";
 
 const getSiteMetaData = () => {
   return {
